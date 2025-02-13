5fa3494ffa1752da696ccb4c717602c7aa7da4e1
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-13 23:00:07 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+})
+
+app.listen(8000, () => console.log(`Server is running on PORT:8000`));
