42fe403731ffabb833cb707ca47e5dca9532f708
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-17 11:56:18 +0530

    Implemented Responsive UI design for the website with all the required components

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: [
+      "light", "acid", {
+        dracula: {
+          ...daisyUIThemes["dracula"],
+          primary: "rgb(29, 155, 240)",
+          // secondary:"rgb(251 236 152)"
+        }
+      },
+      {
+        black: {
+          ...daisyUIThemes["black"],
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+        },
+      },
+    ],
+  },
+};
