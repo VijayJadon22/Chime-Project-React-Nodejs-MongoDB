16a0f4eeed3c02c98bde2d746fedede157cd343f
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-26 16:42:10 +0530

    Preapred and modified few section of app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"; // Importing the Express library to create a web server
 import dotenv from "dotenv"; // Importing the dotenv library to load environment variables from a .env file
 import cookieParser from "cookie-parser";
@@ -22,6 +23,7 @@ cloudinary.config({
 
 const app = express(); // Creating an instance of the Express application
 const PORT = process.env.PORT || 5000; // Defining the port number to listen on, using the value from the environment variable or 5000 as default
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // Middleware to parse incoming JSON requests
 //keep limit uptil 5mb only otherwise server might crash due to denial of service (dos)
@@ -33,6 +35,14 @@ app.use("/api/auth", authRoutes); // Setting up the authentication routes with t
 app.use("/api/users", userRoutes); // Setting up the user routes with the base URL "/api/users"
 app.use("/api/posts", postRoutes); // Setting up the post routes with the base URL "/api/posts"
 app.use("/api/notifications", notificationRoutes); //Setting up the notfication routes with the base URL "/api/notifications"
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 // Starting the server and listening on the defined port
 app.listen(PORT, () => {
